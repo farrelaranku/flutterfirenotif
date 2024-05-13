21fd7c3fe5c0483e88cab77ad7d5d59cbc398169
farrelaranku <flinkin16@gmail.com>
2024-05-13 14:44:46 +0700

    first push

---
+++ lib/api/firebase_api.dart
@@ -0,0 +1,45 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:pushnotif/main.dart';
+
+class FirebaseApi{
+
+
+  //create instance firebase messaging
+  final _firebaseMessaging  = FirebaseMessaging.instance;
+
+
+  //function initialize notif
+  Future<void> initNotifications() async{
+    //req permission from user
+  await _firebaseMessaging.requestPermission();
+    //fetch the FCM token for device
+  final fcmToken = await _firebaseMessaging.getToken();
+
+  //print token
+  print ('Token = $fcmToken');
+
+  initPushNotification();
+  }
+
+  //handle received messages
+  void handleMessage(RemoteMessage? message){
+    //message null do nothing
+    if (message==null) return;
+
+    //navigate to new screen when receive message and user tap
+    navigatorKey.currentState?.pushNamed(
+      '/notification_screen',
+      arguments:message,
+    );
+  }
+
+  //init background settings
+  Future initPushNotification() async{
+    //handle notif if the app was terminated and opened
+    FirebaseMessaging.instance.getInitialMessage().then(handleMessage);
+
+    //attach event listeners for when a notif open app
+    FirebaseMessaging.onMessageOpenedApp.listen(handleMessage);
+  }
+
+}
---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:pushnotif/api/firebase_api.dart';
+import 'package:pushnotif/firebase_options.dart';
+import 'package:pushnotif/pages/notification_page.dart';
+import 'pages/home_page.dart';
+import 'package:firebase_core/firebase_core.dart';
+final navigatorKey = GlobalKey<NavigatorState>();
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  await FirebaseApi().initNotifications();
+  runApp(const MyApp());
+}
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+      navigatorKey: navigatorKey,
+      routes: {
+        '/notification_screen': (context) => const NotificationPage(),
+      },
+    );
+  }
+}
